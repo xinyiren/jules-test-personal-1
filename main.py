cecc23d6384cd10c10956f26568ab521d587db02
xinyiren <xinyiren@berkeley.edu>
2025-07-29 17:32:36 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Simple Hello World Python Program
+
+This script contains a main function that prints "Hello World" to the console.
+"""
+
+
+def main():
+    """
+    Main function that prints Hello World.
+    """
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main() 
